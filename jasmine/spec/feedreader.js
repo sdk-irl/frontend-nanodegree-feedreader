c0c7e677b66820a08ba3c777ccdcd671cf6de440
Susanna Kline <susannakline@gmail.com>
2018-08-29 22:51:52 -0500

    Add initial entry async test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,18 +9,9 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        /* Tests to make sure the allFeeds variable has been defined 
+         * and that it is not empty. */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -49,26 +40,24 @@ $(function() {
         });
     });
 
-
-    /* test suite named "The menu" */
     describe('The menu', function() {
+        const body = document.querySelector('body');
         /* Test that the menu element is hidden by default. */
         it('hides the menu element by default', function() {
-            const body = querySelectory('body');
-            expect(body.classList).contains('menu-hidden').toBe(true);
+            expect(body.classList.contains('menu-hidden')).toBe(true);
         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-         it('toggles visibility when the menu icon is clicked', function() {
-
-         });
+         /* Test that menu changes visibility when the menu icon is clicked: 
+          * does the menu display when clicked and does it hide when clicked again. */
+        it('toggles visibility when the menu icon is clicked', function() {
+            const menuIcon = document.querySelector('.menu-icon-link');    
+            menuIcon.click();
+            expect(body.classList.contains('menu-hidden')).toBe(false);
+            menuIcon.click();
+            expect(body.classList.contains('menu-hidden')).toBe(true);
+        });
      });
 
-    /* test suite named "Initial Entries" */
     describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -76,21 +65,33 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        it('are in the feed container when the loadfeed is called', function(
+         
+        beforeEach(function(done) {
+            loadFeed(0, done);
+            });
 
-        });
+        it('are in the feed container when the loadfeed is called', function() {
+            const feed = document.querySelector('.feed');
+            expect(feed.children.length>0).toBe(true);
+        });  
     });
 
-    /* test suite named "New Feed Selection" */
+
+
     describe('New Feed Selection', function() {
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
-        it('loads new content', function()
+            const feed = document.querySelector('feed');
+         /* TODO: Write a test that ensures when a new feed is loaded
+          * by the loadFeed function that the content actually changes.
+          * Remember, loadFeed() is asynchronous.
+          */
+        beforeEach(function(done) {
+            loadFeed(0);
+            loadFeed(1, done);
+        });
         
-        )};
+        it('loads new content', function() {
+    
+         });
     });
-
-}());
+      
+});
